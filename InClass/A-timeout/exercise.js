9543795ec803ba7ff82bc9ecf8a17e7f721e7449
amadou saidy <ahmo300@outlook.com>
2020-05-06 19:49:38 +0200

    i did the A-timeout exercise

--- InClass/A-timeout/exercise.js
+++ InClass/A-timeout/exercise.js
@@ -9,3 +9,21 @@ Task 2
 Update your code to make the colour change every 5 seconds to something different. Hint: try searching for setInterval. (https://www.w3schools.com/jsref/met_win_setinterval.asp)
 ================
 */
+// task 1
+
+var background = document.querySelector("body");
+setTimeout(function () {
+    background.style.backgroundColor = "red"
+}, 5000);
+
+// task 2
+var background = document.querySelector("body");
+var colors = ["red", "blue", "green", "yellow"];
+var counter = 0;
+setInterval(function () {
+    background.style.backgroundColor = colors[counter];
+    counter++;
+    if(counter === colors.length) {
+    counter = 0;
+    }
+}, 5000);
